86e4ff9522e2e3eb2b2863823048cae91446e9a9
Mobin Fazli <126828525+MobinFazli@users.noreply.github.com>
2024-02-02 17:43:47 -0500

    Joke Generator

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-// Get the HTML element with the ID jokes and store it in the variable jokeElement
+// Get the HTML element with the ID jokes and store it in the variable jokeElement!
 const jokeElement = document.getElementById('jokes')
 
-// Get the HTML element with the ID jokesBtn and store it in the variable jokesBtn
+// Get the HTML element with the ID jokesBtn and store it in the variable jokesBtn!
 const jokesBtn = document.getElementById('jokesBtn')
 
-// Add an event listener to the jokesBtn element, to call the jokeGenerator
+// Add an event listener to the jokesBtn element, to call the jokeGenerator!
 jokesBtn.addEventListener('click', jokeGenerator)
 
-// Define an asynchronous function
+// Define an asynchronous function:
 async function jokeGenerator() {
-    // Configuration object for the fetch request, showing that the response should be in JSON format
+    // Configuration object for the fetch request, showing that the response should be in JSON format!
     const config = {
         headers: {
             Accept: 'application/json',
         },
     }
 
-    // Fetch jokes from the specified website using the configured settings
+    // Fetch jokes from the specified website using the configured settings!
     const res = await fetch('https://icanhazdadjoke.com', config)
 
-    // Extract JSON data from the fetched response
+    // Extract JSON data from the fetched response!
     const data = await res.json()
 
-    // Set the inner HTML content of the jokeElement to the joke retrieved from the data
+    // Set the inner HTML content of the jokeElement to the joke retrieved from the data!
     jokeElement.innerHTML = data.joke
 }
 
-// Call the jokeGenerator function
+// Call the jokeGenerator function!
 jokeGenerator()
